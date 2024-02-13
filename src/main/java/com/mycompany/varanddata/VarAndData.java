f238ec907cda90c68eb995ad1bb5d9a2c8f90537
HP <HP@DESKTOP-8C42MLL>
2024-02-13 14:45:17 +0800

    version 1.0

---
+++ src/main/java/com/mycompany/varanddata/VarAndData.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.varanddata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class VarAndData {
+
+    public static void main(String[] args) {
+        
+//        String fullName = "Jo";
+//        int age =17;
+//        
+//          System.out.println(fullName + " " + "is" + age + "young");      
+        
+          
+//          String firstName = "Jo";
+//          String lastName =   "March" ;
+//          char Gender =  'F';
+//          int age = 17;
+//          
+//          System.out.println(firstName + " " + lastName);
+////          System.out.println("Gender: " + Gender);  
+////          System.out.println("Age: " + age);
+//            int numOne = 77;
+//            int numTwo = 33 ;
+//            int numThree = 12 ;
+//            int j;
+//            j = numOne + numTwo + numThree;
+//            System.out.println(j);
+                    
+//            Scanner scn = new Scanner (System.in) ;
+//       
+//        System.out.println("Enter number1");
+//        int num1 = scn.nextInt();
+//       
+//        System.out.println("Enter number2");
+//         int num2 = scn.nextInt() ;
+//        
+//         System.out.println("Enter number3");
+//        int num3 = scn.nextInt();
+//        
+//        int Total = num1 + num2 + num3;
+//        
+//        
+//        System.out.println("Total of this number is: " + Total);
+          
+//          int radius = 10;
+//          double pi = Math.PI;
+//          double equation = pi * radius * radius ;
+//          
+//          System.out.println("Area of the circle: " + equation);
+                  
+    }
+}
